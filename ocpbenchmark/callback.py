3510bc6a2764c53e1fe5c089c5f333eeade2a322
JianghanZHang <zhangjianghan2@gmail.com>
2024-04-11 16:19:32 -0400

    Added biped.py for bipedal problems

--- ocpbenchmark/callback.py
+++ ocpbenchmark/callback.py
@@ -1,14 +1,16 @@
 import crocoddyl
 
 class benchmark_callback(crocoddyl.CallbackAbstract):
-    def __init__(self, attributes_name):
+    def __init__(self, attributes_config):
         super().__init__()
-        self.KKTs = [] #KKT conditions
-        self.grads = [] # gradients   
-        self.ffess = [] # dynamics infeasibilities
-        self.gfess = [] # ineqaulity infeasibilities
-        self.hfess = [] # equality infeasibilities
-        self.merits = [] # merit function values
-        self.steps = [] # step lengths
+        self.attributes_config = attributes_config
+        self.data = {key: [] for key in attributes_config.keys()}
+
     def __call__(self, solver):
-        self.problem.callback(solver)
+        for attr, solver_attr in self.attributes_config.items():
+            # Check if the solver has the attribute. If not, raise an exception.
+            if not hasattr(solver, solver_attr):
+                raise AttributeError(f"Solver does not have the attribute '{solver_attr}'.")
+            # Otherwise, record the attribute's value.
+            value = getattr(solver, solver_attr)
+            self.data[attr].append(value)
--- ocpbenchmark/problem.py
+++ ocpbenchmark/problem.py
@@ -2,13 +2,75 @@
 Return a predefined shooting problem instance.
 """
 import crocoddyl
+import callback
+from problems.biped import SimpleBipedGaitProblem
+import example_robot_data
+import pinocchio
+import numpy as np
 
 def make_problem(problem_config):
     initial_state = problem_config['initial_state']
+    initial_guess = problem_config['initial_guess']
     horizon = problem_config['horizon']
     weights = problem_config['weights']
     robot_type = problem_config['robot_type']
     problem_type = problem_config['problem_type']
+    dt = problem_config['dt']
+    stop = problem_config['stop']
     if robot_type == 'bipedal':
+        talos_legs = example_robot_data.load("talos_legs")
+        # Defining the initial state of the robot
+        q0 = talos_legs.model.referenceConfigurations[initial_state].copy()
+        v0 = pinocchio.utils.zero(talos_legs.model.nv)
+        x0 = np.concatenate([q0, v0])
+
+        # Setting up the 3d walking problem
+        rightFoot = "right_sole_link"
+        leftFoot = "left_sole_link"
+        gait = SimpleBipedGaitProblem(talos_legs.model, rightFoot, leftFoot)
+
+        GAITPHASES = [
+            {
+                "walking": {
+                    "stepLength": 0.6,
+                    "stepHeight": 0.1,
+                    "timeStep": 0.03,
+                    "stepKnots": 35,
+                    "supportKnots": 10,
+                }
+            },
+            {
+                "jumping": {
+                    "jumpHeight": 0.15,
+                    "jumpLength": [0.0, 0.3, 0.0],
+                    "timeStep": 0.03,
+                    "groundKnots": 10,
+                    "flyingKnots": 20,
+                }
+            }  
+        ]
         
+        if problem_type == "walking":
+            # Creating a walking problem
+                return gait.createWalkingProblem(
+                    x0,
+                    GAITPHASES[problem_type]["stepLength"],
+                    GAITPHASES[problem_type]["stepHeight"],
+                    GAITPHASES[problem_type]["timeStep"],
+                    GAITPHASES[problem_type]["stepKnots"],
+                    GAITPHASES[problem_type]["supportKnots"],
+                )
+        elif problem_type == "jumping":
+             return gait.createJumpingProblem(
+                    x0,
+                    GAITPHASES[problem_type]["stepLength"],
+                    GAITPHASES[problem_type]["stepHeight"],
+                    GAITPHASES[problem_type]["timeStep"],
+                    GAITPHASES[problem_type]["stepKnots"],
+                    GAITPHASES[problem_type]["supportKnots"],
+                )
+
+
+
+                
 
