e77a10b54d0b0cbf38353c38111f94968f514c92
JianghanZHang <zhangjianghan2@gmail.com>
2024-04-15 17:28:02 -0400

    Added initial conditions generator

--- ocpbenchmark/initial.py
+++ ocpbenchmark/initial.py
@@ -1,6 +1,10 @@
 import problem
 import crocoddyl
 import numpy as np
+import mim_solvers
+
+#set seed for reproducibility
+np.random.seed(0)
 
 def make_initial(problem, initial_configs):
     """
@@ -16,9 +20,21 @@ def make_initial(problem, initial_configs):
     T = problem.T
 
     if initial_configs['type'] == 'quasi-static':
-        xs_init = np.ones((T+1, problem.ndx))
+        xs_init = np.ones((T+1, problem.ndx)) * problem.x0
         us_init = problem.quasiStatic(xs_init)
     
     elif initial_configs['type'] == 'random':
+        xs_init = np.random.uniform(initial_configs['state_lb'], initial_configs['state_ub'], (T+1, problem.ndx))
+        us_init = np.random.uniform(initial_configs['control_lb'], initial_configs['control_ub'], (T, problem.nu))
 
-    return xs_init, us_init
+    elif initial_configs['type'] == 'warm-start':
+        xs_init = np.ones((T+1, problem.ndx)) * problem.x0
+        us_init = problem.quasiStatic(xs_init)
+        
+        solver = mim_solvers.SolverCSQP(problem)
+        solver.solve(xs_init, us_init, 10000)
+        xs_init = list(solver.xs[1:]) + [solver.xs[-1]]
+        xs_init[0] = problem.x0
+        us_init = list(solver.us[1:]) + [solver.us[-1]] 
+
+    return list(xs_init), list(us_init)
