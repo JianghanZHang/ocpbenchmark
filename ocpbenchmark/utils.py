e14454b3ed6dd680b1f18e0533336bee5f2ba19c
JianghanZHang <zhangjianghan2@gmail.com>
2024-04-18 18:43:14 -0400

    .

--- ocpbenchmark/utils.py
+++ ocpbenchmark/utils.py
@@ -4,7 +4,7 @@ def load_config_file(EXP_NAME, path_prefix=''):
     '''
     Load YAML config file corresponding to an experiment name
     '''
-    config_path = os.path.join(path_prefix, 'config/'+EXP_NAME+".yml")
+    config_path = os.path.join(path_prefix, 'configs/'+EXP_NAME+".yml")
     with open(config_path) as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
     return data
