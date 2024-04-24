e7b66500e8f15d92043c33f657f383db3ff2d39c
JianghanZHang <zhangjianghan2@gmail.com>
2024-04-24 17:47:44 -0400

    included ddp

--- ocpbenchmark/solver.py
+++ ocpbenchmark/solver.py
@@ -22,11 +22,20 @@ def make_solver(problem, solver_config):
         # QP termination tolerance
         solver.eps_abs = float(config['eps_abs'])
         solver.eps_rel = float(config['eps_rel'])
+        solver.max_qp_iters = int(config['max_qp_iters'])
         
         solver.use_filter_line_search = bool(config['use_filter_line_search']) 
 
-        solver.with_callbacks = True
-        
+        solver.setCallbacks([mim_solvers.CallbackVerbose("CSQP")])
+
+    elif config['solver'] == 'sqp':
+        solver = mim_solvers.SolverSQP(problem)
+        solver.setCallbacks([mim_solvers.CallbackVerbose('SQP')])
+
+    elif config['solver'] == 'ddp':
+        import crocoddyl
+        solver = crocoddyl.SolverDDP(problem)
+        solver.setCallbacks([crocoddyl.CallbackVerbose()])
     else:
         raise ValueError(f"Solver '{config['solver']}' not implemented.")
     
@@ -49,11 +58,13 @@ def make_solver(problem, solver_config):
         xs_init = [problem.x0]*(problem.T + 1)
         us_init = problem.quasiStatic([problem.x0] * problem.T) 
 
-        solver = mim_solvers.SolverCSQP(problem)
-        solver.solve(xs_init, us_init, 10000)
-        xs_init = list(solver.xs[1:]) + [solver.xs[-1]]
-        xs_init[0] = problem.x0
-        us_init = list(solver.us[1:]) + [solver.us[-1]] 
+        warm_starter = mim_solvers.SolverSQP(problem)
+        warm_starter.solve(xs_init, us_init, 100)
+        xs_init = list(warm_starter.xs)
+        us_init = list(warm_starter.us)
+        # xs_init = list(solver.xs[1:]) + [solver.xs[-1]]
+        # xs_init[0] = problem.x0
+        # us_init = list(solver.us[1:]) + [solver.us[-1]] 
     else:
         raise ValueError(f"Initial guess '{config['type']}' not implemented.")
 
